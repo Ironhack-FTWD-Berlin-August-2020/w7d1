1f30116d510b65b8b40e29681713c40716039f15
Jan Redmann <janredmann1@gmail.com>
2020-09-21 16:04:12 +0200

    add components and props

--- components-props/src/index.js
+++ components-props/src/index.js
@@ -1,16 +1,37 @@
-import React, { Component } from 'react';
+import React, { Fragment } from 'react';
 import ReactDOM from 'react-dom';
+import App from './App.js';
 
-// class component
-class Greeting extends Component {
-    render() {
-        return (
-            <h1>Hello World</h1>
-        )
-    }
-}
+// class App extends React.Component {
+//     render() {
+//         return (
+//             <Fragment>
+//                 <Greeting person='Marty' />
+//                 <Greeting person='Biff' />
+//             </Fragment>
+//         )
+//     }
+// }
+
+// Class component
+// class Greeting extends React.Component {
+//     render() {
+//         console.log(this.props);
+//         return (
+//             <div>
+//                 <h1>Hello {this.props.person}</h1>
+//                 <h1>Hello {this.props.anotherPerson}</h1>
+//             </div>
+//         )
+//     }
+// }
+
+// Functional component
+// const Greeting = (props) => {
+//     return <h1>Hello {props.person}</h1>
+// }
 
 ReactDOM.render(
-    <Greeting />,
+    <App />,
     document.getElementById('root')
 )
